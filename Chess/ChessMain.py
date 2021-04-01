5fb7e29eaf64a61cabe7ce0df226ca20af0361c4
AAA530 <dhairyapatelad@gmail.com>
2021-04-01 23:23:51 +0530

    made pieces move

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -1,7 +1,5 @@
 import sys
 sys.path.append('')
-
-
 from Chess import ChessEngine
 import pygame as p
 
@@ -21,7 +19,7 @@ def loadImages():
 
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load(
-            "images/"+piece+".png"), (SQUARE_SIZE, SQUARE_SIZE))
+            "Chess/images/"+piece+".png"), (SQUARE_SIZE, SQUARE_SIZE))
 
 
 def main():
@@ -30,8 +28,62 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color('white'))
     gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    sqSelected = ()
+    playerClicks = []
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0]//SQUARE_SIZE
+                row = location[1]//SQUARE_SIZE
 
-    print(gs.board)
+                if sqSelected == (row, col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = ChessEngine.Move(
+                        playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = []
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
 
 
-main()
+def drawGameState(screen, gs):
+    drawBoard(screen)  # draw square on board
+    # piece sugesstion for later
+    drawPieces(screen, gs.board)  # draw pieces on top of board
+
+
+def drawBoard(screen):
+    colors = [p.Color('white'), p.Color("gray")]
+
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r+c) % 2]
+            p.draw.rect(screen, color, p.Rect(c*SQUARE_SIZE, r *
+                                              SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != '--':
+                screen.blit(IMAGES[piece], p.Rect(c*SQUARE_SIZE, r *
+                                                  SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+
+if __name__ == "__main__":
+    main()
