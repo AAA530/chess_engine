3c78492dbadf3ddb4d09ae3c712bc4d1268e6c09
AAA530 <dhairyapatelad@gmail.com>
2021-04-05 00:02:42 +0530

    added all rules for moves

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -91,9 +91,6 @@ class GameState():
             for i in range(1, 8):
                 endRow = r + d[0] * i
                 endCol = c + d[1] * i
-                print("************************")
-                print(endRow)
-                print(endCol)
                 if 0 <= endRow < 8 and 0 <= endCol < 8:
 
                     endPiece = self.board[endRow][endCol]
@@ -110,33 +107,20 @@ class GameState():
                     break
 
     def getKnightMoves(self, r, c, moves):
-        pass
+        knightMoves = ((-2, -1), (-2, 1), (2, -1), (2, 1),
+                       (1, 2), (1, -2), (-1, 2), (-1, -2))
+        allycolor = "w" if self.whiteToMove else "b"
+        for m in knightMoves:
+            endRow = r + m[0]
+            endCol = c + m[1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allycolor:
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
 
     def getQueenMoves(self, r, c, moves):
-        directions = ((-1, -1), (1, -1), (-1, 1), (1, 1),
-                      (-1, 0), (0, -1), (1, 0), (0, 1))
-        enemyColor = "b" if self.whiteToMove else "w"
-        for d in directions:
-            for i in range(1, 8):
-                endRow = r + d[0] * i
-                endCol = c + d[1] * i
-                print("************************")
-                print(endRow)
-                print(endCol)
-                if 0 <= endRow < 8 and 0 <= endCol < 8:
-
-                    endPiece = self.board[endRow][endCol]
-                    if endPiece == "--":
-                        moves.append(
-                            Move((r, c), (endRow, endCol), self.board))
-                    elif endPiece[0] == enemyColor:
-                        moves.append(
-                            Move((r, c), (endRow, endCol), self.board))
-                        break
-                    else:
-                        break
-                else:
-                    break
+        self.getRookMoves(r, c, moves)
+        self.getBishopMoves(r, c, moves)
 
     def getBishopMoves(self, r, c, moves):
         directions = ((-1, -1), (1, -1), (-1, 1), (1, 1))
@@ -145,9 +129,6 @@ class GameState():
             for i in range(1, 8):
                 endRow = r + d[0] * i
                 endCol = c + d[1] * i
-                print("************************")
-                print(endRow)
-                print(endCol)
                 if 0 <= endRow < 8 and 0 <= endCol < 8:
 
                     endPiece = self.board[endRow][endCol]
@@ -164,7 +145,16 @@ class GameState():
                     break
 
     def getKingMoves(self, r, c, moves):
-        pass
+        kingMoves = ((-1, -1), (1, -1), (-1, 1), (1, 1),
+                     (-1, 0), (0, -1), (1, 0), (0, 1))
+        allycolor = "w" if self.whiteToMove else "b"
+        for i in range(8):
+            endRow = r + kingMoves[i][0]
+            endCol = c + kingMoves[i][1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allycolor:
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
 
 
 class Move():
