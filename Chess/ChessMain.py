6ca4ce2af8409a18ee11c737bc08b014c2e57265
AAA530 <dhairyapatelad@gmail.com>
2021-04-30 22:30:18 +0530

    added Gameover and piece animation

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -4,7 +4,7 @@ from Chess import ChessEngine
 import pygame as p
 
 
-WIDTH = HEIGHT = 512
+WIDTH = HEIGHT = 800
 
 DIMENSION = 8
 
@@ -30,50 +30,77 @@ def main():
     gs = ChessEngine.GameState()
     loadImages()
     running = True
+    animate = False
     validMoves = gs.getAllValidMoves()
     moveMade = False
 
     sqSelected = ()
     playerClicks = []
+
+    gameOver = False
+
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos()
-                col = location[0]//SQUARE_SIZE
-                row = location[1]//SQUARE_SIZE
+                if not gameOver:
+                    location = p.mouse.get_pos()
+                    col = location[0]//SQUARE_SIZE
+                    row = location[1]//SQUARE_SIZE
 
-                if sqSelected == (row, col):
-                    sqSelected = ()
-                    playerClicks = []
-                else:
-                    sqSelected = (row, col)
-                    playerClicks.append(sqSelected)
-                if len(playerClicks) == 2:
-                    move = ChessEngine.Move(
-                        playerClicks[0], playerClicks[1], gs.board)
-                    print(move.getChessNotation())
+                    if sqSelected == (row, col):
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        sqSelected = (row, col)
+                        playerClicks.append(sqSelected)
+                    if len(playerClicks) == 2:
+                        move = ChessEngine.Move(
+                            playerClicks[0], playerClicks[1], gs.board)
+                        print(move.getChessNotation())
 
-                    for i in range(len(validMoves)):
-                        if(move == validMoves[i]):
-                            gs.makeMove(validMoves[i])
-                            moveMade = True
-                            sqSelected = ()
-                            playerClicks = []
-                    if not moveMade:
-                        playerClicks = [sqSelected]
+                        for i in range(len(validMoves)):
+                            if(move == validMoves[i]):
+                                gs.makeMove(validMoves[i])
+                                moveMade = True
+                                animate = True
+                                sqSelected = ()
+                                playerClicks = []
+                        if not moveMade:
+                            playerClicks = [sqSelected]
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:
                     print("Z")
                     gs.undoMove()
                     moveMade = True
+                    animate = False
+                if e.key == p.K_r:
+                    gs = ChessEngine.GameState()
+                    validMoves = gs.getAllValidMoves()
+                    sqSelected = ()
+                    playerClicks = []
+                    moveMade = False
+                    animate = False
 
         if moveMade:
+            if animate:
+                animateMove(gs.moveLog[-1], screen, gs.board, clock)
             validMoves = gs.getAllValidMoves()
             moveMade = False
 
         drawGameState(screen, gs, validMoves, sqSelected)
+
+        if gs.checkMate:
+            gameOver = True
+            if gs.whiteToMove:
+                drawText(screen, 'Black Wins by checkmate')
+            else:
+                drawText(screen, "White wins by Checkmate")
+        elif gs.staleMate:
+            gameOver = True
+            drawText(screen, "Game Drawn")
+
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -105,6 +132,7 @@ def drawGameState(screen, gs, validMoves, sqSelected):
 
 
 def drawBoard(screen):
+    global colors
     colors = [p.Color(235, 235, 208), p.Color(119, 148, 85)]
 
     for r in range(DIMENSION):
@@ -121,6 +149,40 @@ def drawPieces(screen, board):
             if piece != '--':
                 screen.blit(IMAGES[piece], p.Rect(c*SQUARE_SIZE, r *
                                                   SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+
+def animateMove(move, screen, board, clock):
+    global colors
+    coords = []
+    dR = move.endRow - move.startRow
+    dC = move.endCol - move.startCol
+    framesPerSquare = 10
+    frameCount = (abs(dR) + abs(dC)) * framesPerSquare
+    for frame in range(frameCount+1):
+        r, c = (move.startRow+dR*frame/frameCount,
+                move.startCol + dC*frame/frameCount)
+        drawBoard(screen)
+        drawPieces(screen, board)
+        color = colors[(move.endRow+move.endCol) % 2]
+        endSquare = p.Rect(move.endCol*SQUARE_SIZE, move.endRow *
+                           SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+        p.draw.rect(screen, color, endSquare)
+
+        if move.pieceCaptured != '--':
+            screen.blit(IMAGES[move.pieceCaptured], endSquare)
+
+        screen.blit(IMAGES[move.pieceMoved], p.Rect(
+            c*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+        p.display.flip()
+        clock.tick(120)
+
+
+def drawText(screen, text):
+    font = p.font.SysFont("Aerial", 64, True, False)
+    textObject = font.render(text, 0, p.Color('Black'))
+    textLocation = p.Rect(0, 0, WIDTH, HEIGHT).move(
+        WIDTH/2 - textObject.get_width()/2, HEIGHT/2 - textObject.get_height()/2)
+    screen.blit(textObject, textLocation)
 
 
 if __name__ == "__main__":
